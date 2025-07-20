db0404588c4fe183e1fc21f4a93ea01322f034f0
harsh vekariya <harshvekariya910@gmail.com>
2025-07-20 12:07:26 +0530

    temp

--- src/config/mlflow_config.py
+++ src/config/mlflow_config.py
@@ -5,4 +5,5 @@ import mlflow
 load_dotenv()  # Loads from .env file
 
 def setup_mlflow():
-    mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+    # mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+    mlflow.set_tracking_uri("http://ec2-16-171-36-111.eu-north-1.compute.amazonaws.com:5000")
