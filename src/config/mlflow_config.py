1d5f00cfae4ad4ab6a1bc791644dc56f9662aca0
harsh vekariya <harshvekariya910@gmail.com>
2025-07-25 17:07:13 +0530

    final deployment of this project

--- src/config/mlflow_config.py
+++ src/config/mlflow_config.py
@@ -5,10 +5,8 @@ import mlflow
 load_dotenv()  # Loads from .env file
 
 def setup_mlflow():
-    # mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
-    mlflow.set_tracking_uri("http://ec2-51-20-85-228.eu-north-1.compute.amazonaws.com:5000") # uri in .env looks like this
-
+    mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+    # mlflow.set_tracking_uri("http://ec2-51-20-37-158.eu-north-1.compute.amazonaws.com:5000") # uri in .env looks like this
 
 def setup_gemini():
-    # return os.getenv("GEMINI_API_KEY")
-    return "AIzaSyDStfTRZ2MuOXzH-00_21KegNppcMVmcJc"
+    return os.getenv("GEMINI_API_KEY")
--- yt_chrome_plugin_frontend/secrets.js
+++
@@ -1,1 +0,0 @@
-const API_KEY = "AIzaSyAgmJrE8oqan69HPqizGgjoxWL_oERDA-k";
